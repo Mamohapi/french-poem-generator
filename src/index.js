2020bc9a4cf90486183d6e1b10877616411ab5d0
Mamohapi <felicitynchocho@gmail.com>
2025-02-02 02:04:44 +0200

    Improved the user experience

--- src/index.js
+++ src/index.js
@@ -19,6 +19,10 @@ function generatePoem(event) {
     "You are a romatic poem expert and love to write short poems. Your mission is to generate a four line poem in basic HTML and separate each line with a <br/>. Make sure to follow the user instructions. Don't include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> strong element. ";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating"> Generating a French poem about ${instructionsInput.value} </div>`;
+
   console.log("Generating a poem");
   console.log("Prompt: ${prompt}");
   console.log("Context: ${context}");
